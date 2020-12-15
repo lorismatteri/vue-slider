1eb01fd0532e4bc11b1871e0e9d19a6923e373d8
Loris Matteri <loris.matteri93@gmail.com>
2020-12-15 15:59:15 +0100

    click avanti
    al click sulla freccia in avanti le immagini vanno in avanti

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 //Istruzioni: Ricreare lo slider di immagini ma questa volta con Vue
+let app = new Vue ({
+    el: '#app',
+    data: {
+        counter: 0,
+        images: [
+            './img/bugatti.jpg',
+            './img/model-s.jpg',
+            './img/s60-polestar.png',
+            './img/superveloce.jpg'
+        ]
+    },
+    methods: {
+        next(){
+            this.counter++
+        }
+    }
+})
